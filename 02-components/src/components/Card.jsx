e915df7b75255af8efb41060ae313ef5de230255
Shivraj Nalawade <shivrajnalawade77@gmail.com>
2026-07-29 23:20:56 +0530

    Components & Props

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Shivraj Nalawade</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptatum.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        This is Navbar
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
