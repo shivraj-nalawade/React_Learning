9efd84eda39a722fc61f82c9d201cb7f66b7c347
Shivraj Nalawade <shivrajnalawade77@gmail.com>
2026-08-02 00:07:30 +0530

    Card Project Added

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
 import { Bookmark } from 'lucide-react'
+const Card = (props) => {
 
-const Card = () => {
-  return (
-    <div className="card">
-            
-            <div className="top">
-              <img src="https://imgs.search.brave.com/KvkePuTrHTCF2cJEnwFbpPTy26zdZaawwZ5YREkpQKU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNzMv/OTUzLzAwMC9zbWFs/bC9hbWF6b24tcGF5/LWxvZ28tZnJlZS1w/bmcucG5n" alt="" />
-              <button>Save <Bookmark size={12}/></button>
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
             </div>
-    
-            <div className="center">
-              <h3>Amazon <span>5 days ago</span></h3>
-              <h2>Senior UI/UX Designer</h2>
-              <div>
-                <h4>Part Time</h4>
-                <h4>Senior Level</h4>
-              </div>
-            </div>
-    
-            <div className="bottom" >
-              <div>
+            <div className="bottom">
                 <div>
-                  <h3>$120/hr</h3>
-                  <p>Mumbai, India</p>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
                 </div>
                 <button>Apply Now</button>
-              </div>
             </div>
-          </div>
-  )
+        </div>
+    )
 }
 
 export default Card
